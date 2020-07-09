e9958ba33ada69b2b8a95976322b648559df89ef
Rittik-77 <rittik.bit@gmail.com>
2020-07-09 14:20:27 +0530

    Added functions to search Food adn Workouts

--- Spring_Intellij/healthifier/src/main/java/com/ricky/healthifier/dao/FoodDAO.java
+++ Spring_Intellij/healthifier/src/main/java/com/ricky/healthifier/dao/FoodDAO.java
@@ -4,6 +4,10 @@ import com.ricky.healthifier.entity.food.FoodDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FoodDAO extends JpaRepository<FoodDTO, Integer> {
+
+    Optional<FoodDTO> findFoodDTOByName(String name);
 }
--- Spring_Intellij/healthifier/src/main/java/com/ricky/healthifier/dao/WorkoutDAO.java
+++ Spring_Intellij/healthifier/src/main/java/com/ricky/healthifier/dao/WorkoutDAO.java
@@ -4,6 +4,10 @@ import com.ricky.healthifier.entity.workout.WorkoutDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WorkoutDAO extends JpaRepository<WorkoutDTO, Integer> {
+
+    Optional<WorkoutDTO> findWorkoutDTOByName(String name);
 }
