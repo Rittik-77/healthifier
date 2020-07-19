4425c3c7ffbb870958d972be455019fbfa7ccdfc
Rittik-77 <rittik.bit@gmail.com>
2020-07-19 20:57:22 +0530

    Add header and sidebar

--- Angular_VS/client/src/app/app.module.ts
+++ Angular_VS/client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
